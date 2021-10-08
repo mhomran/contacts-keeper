d82c8b372bb794eb8e631c7479508cf831972db8
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-10-08 16:09:39 +0200

    Lesson 40. Create & Respond With JSON Web Token

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 // @route         POST api/users
@@ -46,7 +48,23 @@ router.post(
 
       await user.save();
 
-      res.send("User saved");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(400).send("Server Error");
